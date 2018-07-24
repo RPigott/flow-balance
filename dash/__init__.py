bee65bc4296abe52439b9f8643ea3cec7a3cca93
rpigott <rpigott@berkeley.edu>
2018-07-24 16:28:48 -0700

    Removed old content

--- dash/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///db/test.db'
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
-
-import dash.views
--- runserver.py
+++
@@ -1,9 +0,0 @@
-#! usr/bin/env python
-
-from dash import app
-
-import sys
-port = int(sys.argv[1])
-
-if __name__ == '__main__':
-	app.run(debug = True, port = port)
